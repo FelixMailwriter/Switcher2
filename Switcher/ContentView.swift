f32e20291c64dceb28bea329037a353dfee9bdd8
andrei arama <emailwriter7@gmail.com>
2020-02-20 02:18:46 +0200

    Initial Commit

---
+++ Switcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Switcher
+//
+//  Created by Arama Andrei on 20.02.2020.
+//  Copyright © 2020 Arama Andrei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
